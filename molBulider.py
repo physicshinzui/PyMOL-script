82d6700548ed117a8d5d8797ab2b5979716c67ea
siida <physicshinzui@gmail.com>
2023-05-31 18:40:41 +0900

    update; for multichains, but failed to cap the 1st chain, but succeed for 2nd one

--- molBulider.py
+++ molBulider.py
@@ -80,13 +80,12 @@ class Builder():
 
         cmd.save(f"{outpdb}")
 
-    def cap(self, object_name, nter_cap="ace", cter_cap="nme", outpdb='capped') -> None:
+    def cap(self, object_name, nter_cap="ace", cter_cap="nme") -> None:
         """
         Args: 
             object_name: String (e.g. an object of chain A)
             nter_cap: "ace"
             cter_cap: "nme" or "nhh"
-            outpdb: 'capped'
         """
         # NOTE: Single chain is ASSUMED in this function. 
                 
@@ -108,21 +107,27 @@ class Builder():
 
         # Clean the data before adding caps.
         # Note that this removes the caps in the object.
-        cmd.remove("not polymer") 
+        cmd.remove(f"not polymer") 
 
         # Their info is used to add caps to them in subsequent lines.  
         (first_resn, first_resi), (last_resn, last_resi) = get_first_last_residue_info(object_name)
 
         # TODO: I wanna put this condition: If the selection below returns zero atom, then raise an error. 
-        # nter_cap is conneted to the object pk1 (Nitrogen)         
-        cmd.edit(object_name+f" and resi {first_resi} and name N") # pk1 is made here 
+        # nter_cap is conneted to the object pk1 (Nitrogen) 
+        nter_sele = f"{object_name} and resi {first_resi} and name N"      
+        print(nter_sele)  
+        cmd.edit(nter_sele) # pk1 is made here 
         editor.attach_amino_acid("pk1", nter_cap)
 
-        # cter_cap is conneted to the object pk1 (Carbon)         
-        cmd.edit(object_name+f" and resi {last_resi} and name C") # pk1 is updated here 
+        # cter_cap is conneted to the object pk1 (Carbon)  
+        cter_sele = f"{object_name} and resi {last_resi} and name C"      
+        print(cter_sele)  
+        cmd.edit(cter_sele) # pk1 is updated here 
         editor.attach_amino_acid("pk1", cter_cap)
-
-        cmd.save(f"{outpdb}.pdb")
+    
+        cmd.unpick()
+        cmd.set_wizard()
+        cmd.refresh_wizard()
 
 def main():
     # TODO: The arguments are in a mess at the moment, so I need to consider what arguments should come.
@@ -142,7 +147,19 @@ def main():
     elif args.mode == "cap":
         object_name = "pdb_obj"
         cmd.load(args.pdb, object_name)
-        
-        builder.cap(object_name=object_name, outpdb="capped")
+
+        chains = cmd.get_chains(object_name)
+        print(f"Chains: {chains}, {len(chains)}")
+        if len(chains)==1:
+            builder.cap(object_name=object_name)
+
+        else:
+            # Capping for each chain
+            for i, chain in enumerate(chains): 
+                chain_obj = f"chain {chain}"
+                cmd.select(chain_obj, f"chain {chain}")
+                builder.cap(object_name=chain_obj)
+            #TODO: The first round chain i.e., chain A cannot be capped for some reason. Why? 
+            cmd.save(f"capped.pdb")
 
 main()
